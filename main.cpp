45d6cd508677afe96223dd8e354989cf4ef8d72e
sabarrett <scottbarrett99@gmail.com>
2024-02-05 18:25:27 -0500

    Create assignment structure + comments

--- main.cpp
+++ main.cpp
@@ -1,53 +1,52 @@
 #include <cstdio>
 #include <iostream>
 #include <ostream>
+#include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
-#include <sstream>
 
 #include <string.h>
 
+#include "allocators.h"
 #include "defer.h"
 #include "socklib.h"
-#include "allocators.h"
 
-std::string do_client(std::istream& in_stream);
-// void do_server(Socket &sock);
+std::string do_client(std::istream &in_stream);
+int run_test();
 
 int main(int argc, char *argv[]) {
-    
-    SockLibInit();
-    defer _shutdown_socklib([]() { SockLibShutdown(); });
-    
-    std::stringstream input("30\n500\n-12\n3.6\nnot a number\n200also not a number\n88.1\ndone");
+  /* TODO: ADD SETUP CODE HERE */
+  SockLibInit();
+  defer _shutdown_socklib([]() { SockLibShutdown(); });
 
-    std::string msg = do_client(input);
-    std::string expected("SORTED -12 3.6 30 500 88.1");
-    if (msg != expected)
-    {
-	std::cout << "TEST FAILED. Expected '" << expected << "' but was '" << msg << "'.\n";
-	exit(1);
-    }
-    else
-    {
-	std::cout << "Test passed 😄\n";
-    }
-
-    return 0;
+  return run_test();
 }
 
-#define STR_ARGS(x) x, sizeof(x) - 1
+int run_test() {
+  std::stringstream input("30\n500\n-12\n3.6\n"
+			  "not a number\n200also not a number\n"
+			  "88.1\ndone");
 
-ByteString& bytestring_append(ByteString &str, const char *c_str) {
-  while (*c_str != '\0') {
-    str.push_back(*c_str++);
+  std::cout << "Testing on input '" << input.str() << "'.\n";
+
+  std::string msg = do_client(input);
+  std::string expected("SORTED -12 3.6 30 500 88.1");
+  std::cout << "Received server message '" << msg << "'.\n";
+
+  if (msg != expected) {
+    std::cout << "TEST FAILED. Expected '" << expected << "' but was '" << msg
+              << "'.\n";
+    return 1;
   }
-  return str;
+  
+  std::cout << "Recieved expected message '" << msg << "'. Test passed 😄\n";
+  return 0;
 }
 
-std::string do_client(std::istream& in_stream) {
-    Socket sock(Socket::Family::INET, Socket::Type::STREAM);
+std::string do_client(std::istream &in_stream) {
+  /* TODO: ADD FUNCTION IMPLEMENTATION HERE */
+  Socket sock(Socket::Family::INET, Socket::Type::STREAM);
 
   Address address("68.183.63.165");
 
@@ -58,39 +57,31 @@ std::string do_client(std::istream& in_stream) {
   std::stringstream message_stream;
   message_stream << "LIST";
 
-  for (std::string line;;)
-  {
-      std::cout << "Enter a number: ";
-      std::cout.flush();
+  for (std::string line;;) {
+    std::cout << "Enter a number: ";
+    std::cout.flush();
 
-      std::getline(in_stream, line);
+    std::getline(in_stream, line);
 
-      if (line == "done")
-	  break;
+    if (line == "done")
+      break;
 
-      if (line.empty() || isspace(line[0]))
-      {
-	  std::cout << "Invalid input.\n";
+    if (line.empty() || isspace(line[0])) {
+      std::cout << "Invalid input.\n";
+    }
+
+    char *end;
+    long i = strtol(line.c_str(), &end, 10);
+    if (end == line.c_str() || *end != '\0') {
+      double d = strtod(line.c_str(), &end);
+      if (end == line.c_str() || *end != '\0') {
+        std::cout << "Invalid input.\n";
+      } else {
+        message_stream << " " << d;
       }
-	  
-      char* end;
-      long i = strtol(line.c_str(), &end, 10);
-      if (end == line.c_str() || *end != '\0')
-      {
-	  double d = strtod(line.c_str(), &end);
-	  if (end == line.c_str() || *end != '\0')
-	  {
-	      std::cout << "Invalid input.\n";
-	  }
-	  else
-	  {
-	      message_stream << " " << d;
-	  }
-      }
-      else
-      {
-	  message_stream << " " << i;
-      }
+    } else {
+      message_stream << " " << i;
+    }
   }
 
   std::string to_send = message_stream.str();
@@ -98,7 +89,7 @@ std::string do_client(std::istream& in_stream) {
 
   printf("Sent %d bytes\n", (int)len);
 
-  static char recv_buffer[1024];
+  static char recv_buffer[4096];
 
   size_t recv_buffer_len = sock.Recv(recv_buffer, sizeof(recv_buffer));
 
@@ -107,30 +98,4 @@ std::string do_client(std::istream& in_stream) {
   std::cout << "' of length " << recv_buffer_len << ".\n";
 
   return std::string(recv_buffer, recv_buffer_len);
-}
-
-void do_server(Socket &sock) {
-  Address address("0.0.0.0");
-
-  sock.Bind(address, 8000);
-
-  printf("Bound socket successfully\n");
-
-  sock.Listen();
-
-  printf("Listened successfully\n");
-
-  Socket connection = sock.Accept();
-
-  printf("Accepted connection!\n");
-
-  ByteString msg;
-  msg.resize(1024);
-
-  connection.Recv(msg);
-
-  std::cout << "Received message of length " << msg.size() << ": '" << msg
-            << "'\n";
-
-  connection.SendAll(STR_ARGS("Hi there, client!"));
 }
